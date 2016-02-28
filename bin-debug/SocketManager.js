865ab4657e552b1fbfa340a9ab1dc787d0d2f12b
a7719665 <a7719665@163.com>
2016-02-28 13:27:37 +0800

    aaaaaaaaaaaa

--- bin-debug/SocketManager.js
+++ bin-debug/SocketManager.js
@@ -16,8 +16,10 @@ var SocketManager = (function () {
     );
     p.startConect = function () {
         this.webSocket = new egret.WebSocket();
+        this.webSocket.type = egret.WebSocket.TYPE_BINARY;
         this.webSocket.addEventListener(egret.ProgressEvent.SOCKET_DATA, this.onReceiveMessage, this);
         this.webSocket.addEventListener(egret.Event.CONNECT, this.onSocketOpen, this);
+        this.webSocket.addEventListener(egret.Event.CLOSE, this.onSocketClose, this);
         this.webSocket.connect("echo.websocket.org", 80);
         //var urlreq: egret.URLRequest = new egret.URLRequest("http://httpbin.org/user-agent");
         //var urlloader: egret.URLLoader = new egret.URLLoader();
@@ -27,11 +29,40 @@ var SocketManager = (function () {
     p.onSocketOpen = function () {
         var cmd = "Hello Egret WebSocket";
         console.log("The connection is successful, send data: " + cmd);
-        this.webSocket.writeUTF(cmd);
+        //this.webSocket.writeUTF(cmd);
+        this.reqTest();
+    };
+    p.onSocketClose = function () {
+        console.log("Socket Close");
     };
     p.onReceiveMessage = function (e) {
-        var msg = this.webSocket.readUTF();
+        var byte = new egret.ByteArray();
+        this.webSocket.readBytes(byte);
+        console.warn("收到消息");
+        var msg = byte.readUTFBytes(byte.length);
         console.log("Receive data:" + msg);
+        var obj = JSON.parse(msg);
+        var type = obj.type;
+        if (type == "getTestSessionRsp") {
+            this.testBack(obj);
+        }
+    };
+    //请求测试
+    p.reqTest = function () {
+        var data = new Object();
+        data.type = "getTestSessionRsp";
+        var byte = new egret.ByteArray();
+        byte.writeUTFBytes(JSON.stringify(data));
+        this.webSocket.writeBytes(byte, 0, byte.bytesAvailable);
+        this.webSocket.flush();
+    };
+    //消息返回 统一处理
+    p.onRegTestClickBack = function () {
+        var data = new Object();
+        data.type = "getTestSessionRsp";
+    };
+    p.testBack = function (obj) {
+        console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     };
     return SocketManager;
 })();
--- src/SocketManager.ts
+++ src/SocketManager.ts
@@ -16,8 +16,10 @@ class SocketManager {
     
     public startConect():void{
         this.webSocket = new egret.WebSocket();
+        this.webSocket.type = egret.WebSocket.TYPE_BINARY;
         this.webSocket.addEventListener(egret.ProgressEvent.SOCKET_DATA,this.onReceiveMessage,this);
         this.webSocket.addEventListener(egret.Event.CONNECT,this.onSocketOpen,this);
+        this.webSocket.addEventListener(egret.Event.CLOSE,this.onSocketClose,this);
         this.webSocket.connect("echo.websocket.org",80);
 
 
@@ -30,11 +32,46 @@ class SocketManager {
     private onSocketOpen(): void {
         var cmd = "Hello Egret WebSocket";
         console.log("The connection is successful, send data: " + cmd);
-        this.webSocket.writeUTF(cmd);
+        //this.webSocket.writeUTF(cmd);
+        this.reqTest();
     }
-
+    private onSocketClose(): void {
+        console.log("Socket Close");
+    }
     private onReceiveMessage(e: egret.Event): void {
-        var msg = this.webSocket.readUTF();
+        var byte:egret.ByteArray = new egret.ByteArray();
+        this.webSocket.readBytes(byte);
+        console.warn("收到消息");
+        
+        var msg :string = byte.readUTFBytes(byte.length);
         console.log("Receive data:" + msg);
+        
+        var obj:any = JSON.parse(msg);
+        var type:string = obj.type;
+        if(type == "getTestSessionRsp"){
+            this.testBack(obj);
+        }
+    }
+    
+    //请求测试
+    private reqTest(): void {
+        var data: any = new Object();
+        data.type = "getTestSessionRsp";
+        
+        var byte:egret.ByteArray = new egret.ByteArray();
+        byte.writeUTFBytes(JSON.stringify(data));
+        this.webSocket.writeBytes(byte,0,byte.bytesAvailable);
+        this.webSocket.flush();
+    }
+    
+    //消息返回 统一处理
+    private onRegTestClickBack(): void {
+        var data: any = new Object();
+        data.type = "getTestSessionRsp";
+
+    }
+    
+    private testBack(obj:any):void{
+        console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     }
 }
